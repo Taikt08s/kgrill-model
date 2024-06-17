f180a0fce0c64b7be01f0465aba5762b8a452124
BuiDuyKhang2003 <huvo201223@gmail.com>
2024-06-17 10:44:49 +0700

    Test CI/Cd

---
+++ src/main/java/com/swd392/group2/kgrill_model/repository/PackageRepository.java
@@ -0,0 +1,8 @@
+package com.swd392.group2.kgrill_model.repository;
+
+import com.swd392.group2.kgrill_model.model.Package;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface PackageRepository extends JpaRepository<Package, Integer> {
+
+}
