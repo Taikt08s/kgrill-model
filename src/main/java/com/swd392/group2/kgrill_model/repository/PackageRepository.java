dbe5761fc94a8aad64b3c2997b192cce256e4abc
BuiDuyKhang2003 <huvo201223@gmail.com>
2024-06-19 11:44:02 +0700

    update model and repo

--- src/main/java/com/swd392/group2/kgrill_model/repository/PackageRepository.java
+++ src/main/java/com/swd392/group2/kgrill_model/repository/PackageRepository.java
@@ -1,8 +1,12 @@
 package com.swd392.group2.kgrill_model.repository;
 
 import com.swd392.group2.kgrill_model.model.Package;
+import lombok.NonNull;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface PackageRepository extends JpaRepository<Package, Integer> {
 
+    Page<Package> findAll(@NonNull Pageable pageable);
 }
