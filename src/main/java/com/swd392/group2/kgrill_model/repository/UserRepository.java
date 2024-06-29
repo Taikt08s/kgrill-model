3988ff4f295597ddbac5100c1e655552a109f44b
Tinhhhh <tinhvvse173112@fpt.edu.vn>
2024-06-29 15:08:27 +0700

    refactor(UserRepository): findByEmail

--- src/main/java/com/swd392/group2/kgrill_model/repository/UserRepository.java
+++ src/main/java/com/swd392/group2/kgrill_model/repository/UserRepository.java
@@ -12,9 +12,12 @@ import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
 import java.util.UUID;
+
 @Repository
 public interface UserRepository extends JpaRepository<User, UUID> {
     Optional<User> findByEmail(String email);
+
+    @Query(value = "SELECT * FROM account WHERE email LIKE %:email%", nativeQuery = true)
     Page<User> findByEmail(String email, Pageable pageable);
 
 }
