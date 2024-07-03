d008f3ebfcc544189e8475904dac891fd561cdff
BuiDuyKhang2003 <huvo201223@gmail.com>
2024-07-03 21:46:15 +0700

    update repo

--- src/main/java/com/swd392/group2/kgrill_model/repository/PackageDishRepository.java
+++ src/main/java/com/swd392/group2/kgrill_model/repository/PackageDishRepository.java
@@ -3,7 +3,9 @@ package com.swd392.group2.kgrill_model.repository;
 import com.swd392.group2.kgrill_model.model.PackageDish;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
 import java.util.Optional;
 
 public interface PackageDishRepository extends JpaRepository<PackageDish, Integer> {
+    List<PackageDish> findAllByPackageEntity_Id(int id);
 }
