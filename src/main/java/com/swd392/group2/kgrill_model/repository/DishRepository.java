7ddda6910c7b95695bcf97458033882850e48ad0
BuiDuyKhang2003 <huvo201223@gmail.com>
2024-07-05 21:55:22 +0700

    update repo

--- src/main/java/com/swd392/group2/kgrill_model/repository/DishRepository.java
+++ src/main/java/com/swd392/group2/kgrill_model/repository/DishRepository.java
@@ -3,5 +3,8 @@ package com.swd392.group2.kgrill_model.repository;
 import com.swd392.group2.kgrill_model.model.Dish;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface DishRepository extends JpaRepository<Dish,Integer> {
+    List<Dish> findByIdIn(List<Integer> ids);
 }
