04882feede5690c78489c530c38ae78450b1dd08
Nope2709 <duongdhse173027@fpt.edu.vn>
2024-07-07 18:51:18 +0700

    update Dish Repository

--- src/main/java/com/swd392/group2/kgrill_model/repository/DishRepository.java
+++ src/main/java/com/swd392/group2/kgrill_model/repository/DishRepository.java
@@ -1,10 +1,17 @@
 package com.swd392.group2.kgrill_model.repository;
 
 import com.swd392.group2.kgrill_model.model.Dish;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 
 public interface DishRepository extends JpaRepository<Dish,Integer> {
     List<Dish> findByIdIn(List<Integer> ids);
+    @Query("SELECT d FROM Dish d WHERE "
+            + "CONCAT(d.name, d.price)"
+            + " LIKE %?1%")
+    Page<Dish> findByNameAndPrice(String keyword, Pageable pageable);
 }
--- src/test/java/com/swd392/group2/kgrill_model/KgrillModelApplicationTests.java
+++ src/test/java/com/swd392/group2/kgrill_model/KgrillModelApplicationTests.java
@@ -3,6 +3,7 @@ package com.swd392.group2.kgrill_model;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+
 @SpringBootTest
 class KgrillModelApplicationTests {
 
