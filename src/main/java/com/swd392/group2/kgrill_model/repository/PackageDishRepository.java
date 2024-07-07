1f94a890e8aa3983532eaae3f006870f107fa6ce
BuiDuyKhang2003 <huvo201223@gmail.com>
2024-07-08 02:19:30 +0700

    update repo

--- src/main/java/com/swd392/group2/kgrill_model/repository/PackageDishRepository.java
+++ src/main/java/com/swd392/group2/kgrill_model/repository/PackageDishRepository.java
@@ -7,5 +7,6 @@ import java.util.List;
 import java.util.Optional;
 
 public interface PackageDishRepository extends JpaRepository<PackageDish, Integer> {
+    List<PackageDish> findAllByPackageEntity_Id(int id);
     PackageDish findByPackageEntity_IdAndDish_Id(int pkgId, int dishId);
-} 
+}
