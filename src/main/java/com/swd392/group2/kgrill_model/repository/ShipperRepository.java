d0f867e0cc30dd5708b6f36f715ea1da00514b49
BuiDuyKhang2003 <huvo201223@gmail.com>
2024-07-20 00:26:17 +0700

    update ShipperRepository

--- src/main/java/com/swd392/group2/kgrill_model/repository/ShipperRepository.java
+++ src/main/java/com/swd392/group2/kgrill_model/repository/ShipperRepository.java
@@ -8,6 +8,9 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
+import java.util.List;
+
 
 public interface ShipperRepository extends JpaRepository<Shipper, Long> {
+    List<Shipper> findAllByStatus(String status);
 }
